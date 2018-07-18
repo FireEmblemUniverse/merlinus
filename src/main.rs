e10fe114999c06013f3cd4c1234433bd43cf5989
Cameron Wong <cwong@purestorage.com>
2018-07-17 18:09:12 -0600

    prep for real server infra

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 
 extern crate toml;
-extern crate git2;
 
 #[macro_use]
 extern crate serde_derive;
@@ -10,11 +9,12 @@ extern crate serde;
 #[macro_use]
 extern crate lazy_static;
 
+extern crate clap;
+
 mod config;
 mod pkgmgr;
 mod backends;
 
 fn main() {
-    println!("Hello, world!");
 }
 
--- src/pkgmgr.rs
+++ src/pkgmgr.rs
@@ -1,35 +1,13 @@
 
-extern crate git2;
-
 use config;
 use std::io;
 use std::fs;
 use std::path;
-
-static FEU_GITHUB_BASE_URL : &'static str =
-    "https://github.com/{USER}/{NAME}.git";
 
 pub fn init_metadata() -> io::Result<()> {
     fs::create_dir(&config::CURRENT_CONFIG.meta_loc)
 }
 
 pub fn fetch_tool((name, cfg): (String, config::ToolVer)) {
-    // XXX - this sucks, stylistically, We could use std::fmt to do it more
-    // nicely, but then we need to have the magic string in the method (instead
-    // of using a static global like above
-    let repo_url =
-        str::replace(
-            str::replace(FEU_GITHUB_BASE_URL,
-                         "{USER}", cfg.owner.as_str()).as_str(),
-            "{NAME}", name.as_str()
-        );
-
-    let mut repo_local = path::PathBuf::new();
-    repo_local.push(&config::CURRENT_CONFIG.meta_loc);
-    repo_local.push(name);
-
-    match git2::Repository::clone(repo_url.as_str(), repo_local) {
-        _ => ()
-    }
 }
 
